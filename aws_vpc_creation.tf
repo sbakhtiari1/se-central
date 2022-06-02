1e8d0259464ccbf589ee72ef08f2275811412018
sbakhtiari1 <106769038+sbakhtiari1@users.noreply.github.com>
2022-06-02 15:09:29 -0500

    Update and rename firenet1.tf to aws_vpc_creation.tf

---
+++ aws_vpc_creation.tf
@@ -0,0 +1,28 @@
+# Configure Aviatrix provider source and version
+terraform {
+  required_providers {
+    aviatrix = {
+      source = "AviatrixSystems/aviatrix"
+      version = "2.20.1"
+    }
+  }
+}
+
+# Configure Aviatrix provider
+provider "aviatrix" {
+  controller_ip           = "35.83.158.223"
+  username                = "admin"
+  password                = "Muhammad1234!"
+  skip_version_validation = false
+  verify_ssl_certificate  = true
+  path_to_ca_certificate  = "/path/to/ca/cert.crt"
+}
+resource "aviatrix_vpc" "aws_vpc" {
+  cloud_type           = 1
+  account_name         = "AWS-Aviatrix"
+  region               = "us-west-1"
+  name                 = "aws-vpc"
+  cidr                 = "10.100.0.0/16"
+  aviatrix_transit_vpc = false
+  aviatrix_firenet_vpc = false
+}
--- firenet1.tf
+++
@@ -1,9 +0,0 @@
-module "transit_firenet_1" {
-  source  = "terraform-aviatrix-modules/aws-transit-firenet/aviatrix"
-  version = "5.0.0"
-
-  cidr           = "10.1.0.0/20"
-  region         = "eu-west-1"
-  account        = "AWS"
-  firewall_image = "Fortinet FortiGate (BYOL) Next-Generation Firewall"
-}
