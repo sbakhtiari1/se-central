ba6ea8c8e4d36c32402a531671e5e38305e76708
sbakhtiari1 <106769038+sbakhtiari1@users.noreply.github.com>
2022-06-10 13:40:51 -0500

    Update aws_vpc_creation.tf

--- aws_vpc_creation.tf
+++ aws_vpc_creation.tf
@@ -15,25 +15,31 @@ provider "aviatrix" {
   password                = "Muhammad1234!"
   skip_version_validation = false
 }
-resource "aviatrix_vpc" "aws_vpc" {
+resource "aviatrix_vpc" "aa1_vpc" {
   cloud_type           = 1
   account_name         = "AWS-Aviatrix"
   region               = "us-west-1"
-  name                 = "aws-vpc"
-  cidr                 = "10.100.0.0/16"
+  name                 = "aa1-vpc"
+  cidr                 = "10.101.0.0/16"
   aviatrix_transit_vpc = false
   aviatrix_firenet_vpc = false
 }
-# Create an Aviatrix AWS Gateway
-resource "aviatrix_gateway" "test_gateway_aws" {
-  cloud_type   = 1
-  account_name = "AWS-Aviatrix"
-  gw_name      = "avtx-gw-1"
-  vpc_id       = "vpc-02ca8503abfcf007a"
-  vpc_reg      = "us-west-1"
-  gw_size      = "t2.micro"
-  subnet       = "10.100.0.0/16"
-  tags         = {
-    name = "value"
-  }
+
+resource "aviatrix_vpc" "aa2_vpc" {
+  cloud_type           = 1
+  account_name         = "AWS-Aviatrix"
+  region               = "us-west-1"
+  name                 = "aa2-vpc"
+  cidr                 = "10.102.0.0/16"
+  aviatrix_transit_vpc = false
+  aviatrix_firenet_vpc = false
+}
+resource "aviatrix_vpc" "aa3_vpc" {
+  cloud_type           = 1
+  account_name         = "AWS-Aviatrix"
+  region               = "us-west-1"
+  name                 = "aa2-vpc"
+  cidr                 = "10.103.0.0/16"
+  aviatrix_transit_vpc = false
+  aviatrix_firenet_vpc = false
 }
