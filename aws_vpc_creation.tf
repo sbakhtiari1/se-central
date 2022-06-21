d5246905df95c0f99c10280ad26897dd163c95bb
sbakhtiari1 <106769038+sbakhtiari1@users.noreply.github.com>
2022-06-21 11:45:36 -0500

    Update aws_vpc_creation.tf

--- aws_vpc_creation.tf
+++ aws_vpc_creation.tf
@@ -52,3 +52,12 @@ resource "aviatrix_vpc" "aa5_vpc" {
   aviatrix_transit_vpc = false
   aviatrix_firenet_vpc = false
 }
+resource "aviatrix_vpc" "aa6_vpc" {
+  cloud_type           = 1
+  account_name         = "AWS-Aviatrix"
+  region               = "us-west-1"
+  name                 = "aa6-vpc"
+  cidr                 = "10.106.0.0/16"
+  aviatrix_transit_vpc = false
+  aviatrix_firenet_vpc = false
+}
