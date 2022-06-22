bd98a757a8749de6a2db8efaac957b139686766a
sbakhtiari1 <106769038+sbakhtiari1@users.noreply.github.com>
2022-06-22 14:57:21 -0500

    Update aws_vpc_creation.tf

--- aws_vpc_creation.tf
+++ aws_vpc_creation.tf
@@ -36,7 +36,7 @@ resource "aviatrix_vpc" "aa10_vpc" {
 }  
 resource "aviatrix_spoke_gateway" "test_spoke_gateway_aws" {
   cloud_type                        = 1
-  account_name                      = "my-aws"
+  account_name                      = "AWS-Aviatrix"
   gw_name                           = "spoke-gw-aws"
   vpc_id                            = "vpc-0dc8bed4b89d53e8c"
   vpc_reg                           = "us-west-1"
