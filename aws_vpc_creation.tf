17a8d473a16ca44884441777ca66ec4a23bb4034
sbakhtiari1 <106769038+sbakhtiari1@users.noreply.github.com>
2022-08-03 22:33:40 -0500

    Update aws_vpc_creation.tf

--- aws_vpc_creation.tf
+++ aws_vpc_creation.tf
@@ -15,58 +15,18 @@ provider "aviatrix" {
   password                = "Muhammad1234!"
   skip_version_validation = false
 }
-module "azure_transit" {
-  source  = "terraform-aviatrix-modules/mc-transit/aviatrix"
-  version = "2.2.0"
-
-  cloud   = "azure"
-  region  = "East US"
-  cidr    = "10.1.0.0/23"
-  account = "Azure-Aviatrix"
+resource "aviatrix_transit_gateway" "test_transit_gateway_azure" {
+  cloud_type        = 8
+  account_name      = "Azure-Aviatrix"
+  gw_name           = "transit"
+  vpc_id            = "vnet_name:rg_name:resource_guid"
+  vpc_reg           = "West US"
+  gw_size           = "Standard_B1ms"
+  subnet            = "10.30.0.0/24"
+  zone              = "az-1"
+  ha_subnet         = "10.30.0.0/24"
+  ha_zone           = "az-2"
+  ha_gw_size        = "Standard_B1ms"
+  connected_transit = true
 }
-module "spoke_azure_1" {
-  source  = "terraform-aviatrix-modules/mc-spoke/aviatrix"
-  version = "1.2.4"
-
-  cloud           = "Azure"
-  name            = "App1"
-  cidr            = "10.1.0.0/20"
-  region          = "East US"
-  account         = "Azure-Aviatrix"
-  transit_gw      = "avx-us-east-transit"
-  security_domain = "green"
-}
-  
-  resource "aviatrix_vpc" "aa7_vpc" {
-  cloud_type           = 1
-  account_name         = "AWS-Aviatrix"
-  region               = "us-west-1"
-  name                 = "aa7-vpc"
-  cidr                 = "10.107.0.0/16"
-  aviatrix_transit_vpc = false
-  aviatrix_firenet_vpc = false
-}
-resource "aviatrix_vpc" "aa10_vpc" {
-  cloud_type           = 1
-  account_name         = "AWS-Aviatrix"
-  region               = "us-west-1"
-  name                 = "aa10-vpc"
-  cidr                 = "10.110.0.0/16"
-  aviatrix_transit_vpc = false
-  aviatrix_firenet_vpc = false
-}  
-resource "aviatrix_spoke_gateway" "test_spoke_gateway_aws" {
-  cloud_type                        = 1
-  account_name                      = "AWS-Aviatrix"
-  gw_name                           = "spoke-gw-aws"
-  vpc_id                            = "vpc-0dc8bed4b89d53e8c"
-  vpc_reg                           = "us-west-1"
-  gw_size                           = "t2.micro"
-  subnet                            = "10.110.32.0/20"
-  single_ip_snat                    = false
-  manage_transit_gateway_attachment = false
-  tags                              = {
-    name = "value"
-  }
-}
-
+ 
